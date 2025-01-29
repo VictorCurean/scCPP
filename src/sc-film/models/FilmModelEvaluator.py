88fac9e37f8391599d2ee8dd6751230071160b65
Victor Curean <victor.curean@unicle.life>
2025-01-29 23:28:15 +0200

    refactor

--- src/sc-film/models/FilmModelEvaluator.py
+++ src/sc-film/models/FilmModelEvaluator.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
 import matplotlib.pyplot as plt
 
-from ..dataset.dataset_sciplex import SciplexDatasetUnseenPerturbations
+from dataset.dataset_sciplex import SciplexDatasetUnseenPerturbations
 from sc_film_v2 import FiLMResidualModel
 
 
