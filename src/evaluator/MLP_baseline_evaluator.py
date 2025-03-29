21366f16aae0a971a4c98b52f8324a37d6a106b8
VictorCurean <curean_victor@yahoo.com>
2025-03-29 11:42:32 +0200

    Remove unused files from the repository
    Deleted obsolete scripts and modules from the `junk/TO_REFACTOR` directory, including Python scripts, Jupyter notebooks, and dataset-related utilities. This cleanup reduces clutter and ensures the repository focuses only on relevant and active components.

--- src/evaluator/MLP_baseline_evaluator.py
+++ src/evaluator/MLP_baseline_evaluator.py
@@ -11,211 +11,109 @@ from tqdm import tqdm
 
 
 class MLPBaselineEvaluator(AbstractEvaluator):
+    def __init__(self, config_path, model, train_dataset, val_dataset, test_dataset, trial):
+        self.config = self.read_config(config_path)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    def __init__(self, config_path, model, sciplex_dataset_train, sciplex_dataset_validation, sciplex_dataset_test, trial):
+        # Initialize model, optimizer, and scheduler
+        self.model = model(self.config).to(self.device)
+        self.test_model = model(self.config).to(self.device)
 
-        self.config = None
-        self.test_results = None
-        self.trained_model_weights = None
-        self.best_epoch = None
-        self.model_patience = None
-        self.scheduler = None
-        self.optimizer = None
-        self.device = None
-        self.best_loss = None
-        
-        
-        self.read_config(config_path)
+        self.optimizer, self.scheduler, self.batch_size, self.model_patience = self.prepare_model(trial)
 
-        #load model
-        self.model = model(self.config)
-        self.test_model = model(self.config)
+        # Load data
+        self.train_loader = self.create_dataloader(train_dataset,  self.batch_size)
+        self.val_loader = self.create_dataloader(val_dataset,  self.batch_size)
+        self.test_loader = self.create_dataloader(test_dataset,  self.batch_size)
 
-        #prepare model
-        self.prepare_model(trial)
-
-        #load training, validation and test data in
-        self.sciplex_loader_train = DataLoader(sciplex_dataset_train,
-                                               batch_size=self.config['train_params']['batch_size'],
-                                               shuffle=True,
-                                               num_workers=0)
-
-        self.sciplex_loader_validation = DataLoader(sciplex_dataset_validation,
-                                               batch_size=self.config['train_params']['batch_size'],
-                                               shuffle=True,
-                                               num_workers=0)
-
-        self.sciplex_loader_test = DataLoader(sciplex_dataset_test,
-                                              batch_size=self.config['train_params']['batch_size'],
-                                              shuffle=True, num_workers=0)
-
-
-
-
-    def read_config(self, config_path):
+    @staticmethod
+    def read_config(config_path):
         with open(config_path, 'r') as file:
-            try:
-                self.config = yaml.safe_load(file)
-            except yaml.YAMLError as exc:
-                print(exc)
-                raise RuntimeError(exc)
+            return yaml.safe_load(file)
 
-
+    @staticmethod
+    def create_dataloader(dataset, batch_size):
+        return DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
     def prepare_model(self, trial):
         lr = trial.suggest_loguniform('lr', 1e-5, 1e-2)
         weight_decay = trial.suggest_loguniform('weight_decay', 1e-6, 1e-2)
-        #batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 128])
         scheduler_factor = trial.suggest_loguniform('scheduler_factor', 0.1, 0.5)
         scheduler_patience = trial.suggest_int('scheduler_patience', 1, 20)
+        batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 128, 256])
 
+        optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                         mode=self.config['train_params']['scheduler_mode'],
+                                                         factor=scheduler_factor,
+                                                         patience=scheduler_patience)
 
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        self.optimizer = optim.Adam(self.model.parameters(),
-                                    lr=lr,
-                                    weight_decay=weight_decay)
-
-        self.model = self.model.to(self.device)
-
-
-        self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,
-                                                              mode=self.config['train_params']['scheduler_mode'],
-                                                              factor=scheduler_factor,
-                                                              patience=scheduler_patience)
-
-
-        self.model_patience = self.config['train_params']['model_patience']
+        model_patience = self.config['train_params']['model_patience']
+        return optimizer, scheduler, batch_size, model_patience
 
     def train(self, loss_fn, trial):
         self.model.train()
-
-        num_epochs = self.config['train_params']['num_epochs']
-
         best_model_weights = self.model.state_dict()
-        self.best_loss = float('inf')
+        best_loss = float('inf')
         epochs_without_improvement = 0
 
-        for epoch in range(num_epochs):
-            for control, drug_emb, target, meta in self.sciplex_loader_train:
-                # Move tensors to the specified device
-                control = control.to(self.device)
-                drug_emb = drug_emb.to(self.device)
-                target = target.to(self.device)
+        for epoch in range(self.config['train_params']['num_epochs']):
+            for control, drug_emb, target, _ in self.train_loader:
+                control, drug_emb, target = control.to(self.device), drug_emb.to(self.device), target.to(self.device)
 
-                # Zero the gradients
                 self.optimizer.zero_grad()
-
-                # Forward pass through the model
-                output = self.model(control, drug_emb)
-
-                # Compute the loss
-                loss = loss_fn(output, target, control)
-
-                # Backprop
+                loss = loss_fn(self.model(control, drug_emb), target, control)
                 loss.backward()
-
-                # Update model parameters
                 self.optimizer.step()
 
             validation_loss = self.validate(loss_fn)
-
             trial.report(validation_loss, epoch)
 
-            if trial.should_prune():
-                raise optuna.exceptions.TrialPruned()
-
-            if validation_loss < self.best_loss:
-                self.best_loss = validation_loss
-                self.best_epoch = epoch + 1
+            if validation_loss < best_loss:
+                best_loss = validation_loss
                 best_model_weights = self.model.state_dict()
                 epochs_without_improvement = 0
             else:
                 epochs_without_improvement += 1
                 if epochs_without_improvement >= self.model_patience:
-                    print(f"Early stopping at epoch {epoch + 1} due to no improvement in validation loss in {self.model_patience} epochs.")
-                    self.trained_model_weights = best_model_weights
-                    return self.best_loss
+                    print(f"Early stopping at epoch {epoch + 1}")
+                    self.model.load_state_dict(best_model_weights)
+                    return best_loss
 
+            if trial.should_prune():
+                raise optuna.exceptions.TrialPruned()
 
-            print(f"Epoch {epoch + 1} Validation Loss:", validation_loss, f"\t| Best loss: {self.best_loss} \t in epoch {self.best_epoch}")
-
-
-        self.trained_model_weights = best_model_weights
-        return self.best_loss
+        self.model.load_state_dict(best_model_weights)
+        return best_loss
 
     def validate(self, loss_fn):
-        validation_losses = list()
+        self.model.eval()
+        validation_losses = []
+
         with torch.no_grad():
-            for control, drug_emb, target, meta in self.sciplex_loader_validation:
-                control, drug_emb, target = (
-                    control.to(self.device),
-                    drug_emb.to(self.device),
-                    target.to(self.device)
-                )
-
-                # Forward pass
-                output_validation = self.model(control, drug_emb)
-
-                # Compute loss
-                validation_loss = loss_fn(output_validation, target, control)
-
-                # Track validation loss
+            for control, drug_emb, target, _ in self.val_loader:
+                control, drug_emb, target = control.to(self.device), drug_emb.to(self.device), target.to(self.device)
+                validation_loss = loss_fn(self.model(control, drug_emb), target, control)
                 validation_losses.append(validation_loss.item())
 
-        self.scheduler.step(np.mean(validation_losses))
-
-        return np.mean(validation_losses)
+        avg_loss = np.mean(validation_losses)
+        self.scheduler.step(avg_loss)
+        return avg_loss
 
     def test(self):
-        control_embeddings = []
-        treated_embeddings = []
-        model_output = []
-        compounds_list = []
-        cell_types_list = []
-        doses_list = []
+        self.model.eval()
+        results = {'ctrl_emb': [], 'pert_emb': [], 'pred_emb': [], 'compound': [], 'cell_type': [], 'dose': []}
 
-        self.test_model = self.test_model.to(self.device)
-        self.test_model.load_state_dict(self.trained_model_weights)
-        self.test_model.eval()
+        with torch.no_grad():
+            for control, drug_emb, target, meta in tqdm(self.test_loader):
+                control, drug_emb, target = control.to(self.device), drug_emb.to(self.device), target.to(self.device)
+                output = self.model(control, drug_emb)
 
-        with torch.no_grad():  # Disable gradient computation
-            for control, drug_emb, target, meta in tqdm(self.sciplex_loader_test):
-                # Move tensors to the specified device
-                control = control.to(self.device)
-                drug_emb = drug_emb.to(self.device)
-                target = target.to(self.device)
+                results['ctrl_emb'].extend(control.cpu().numpy())
+                results['pert_emb'].extend(target.cpu().numpy())
+                results['pred_emb'].extend(output.cpu().numpy())
+                results['compound'].extend(meta['compound'])
+                results['cell_type'].extend(meta['cell_type'])
+                results['dose'].extend([d.item() for d in meta['dose']])
 
-                # Forward pass through the model
-                output = self.test_model(control, drug_emb)
-
-                # Convert tensors to lists of NumPy arrays for DataFrame compatibility
-                control_emb_list = [x.cpu().numpy() for x in torch.unbind(control, dim=0)]
-                treated_emb_list = [x.cpu().numpy() for x in torch.unbind(target, dim=0)]
-                output_list = [x.cpu().numpy() for x in torch.unbind(output, dim=0)]
-
-                # Meta information
-                compounds = meta['compound']
-                cell_types = meta['cell_type']
-                doses = [d.item() for d in meta['dose']]
-
-                # Append results to lists
-                control_embeddings.extend(control_emb_list)
-                treated_embeddings.extend(treated_emb_list)
-                model_output.extend(output_list)
-                compounds_list.extend(compounds)
-                cell_types_list.extend(cell_types)
-                doses_list.extend(doses)
-
-        # Save results into a DataFrame
-        self.test_results = pd.DataFrame({
-            "ctrl_emb": control_embeddings,
-            "pert_emb": treated_embeddings,
-            "pred_emb": model_output,
-            "compound": compounds_list,
-            "cell_type": cell_types_list,
-            "dose": doses_list
-        })
-
-    def get_test_results(self):
-        return self.test_results
+        return pd.DataFrame(results)
